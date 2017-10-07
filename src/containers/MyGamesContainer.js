2c91a1b299c7cf335267ad54a641f59332896bcf
Adamska <adams.roy27@gmail.com>
2017-10-07 07:33:12 +0200

    remove old code

--- src/containers/MyGamesContainer.js
+++ src/containers/MyGamesContainer.js
@@ -1,53 +1,20 @@
 import React, { Component } from 'react'
 import Header from '../components/global/Header'
-import Button from '../components/global/Button'
 import MyGamesList from '../components/MyGamesList'
+import Filters from '../components/Filters'
 
 import { connect } from 'react-redux'
-import { checkGame } from '../Redux/actions'
+import { checkGame, filterMyGames } from '../Redux/actions'
 
 
 class MyGames extends Component {    
     
-    selectFilter(filter, e) {
-        const buttons = document.getElementsByClassName('filter-button')
-        Array.from(buttons).map( button => button.classList.remove("active"))
-        e.target.classList.add("active")
-
-        if (filter === 'played') {
-            const gamesPlayed = this.state.myGames.filter( (game) => game.played)
-            this.setState({gamesPlayed})
-        } else if (filter === 'all') {
-            this.setState({myGames: this.state.myGames})
-        } else if (filter === 'to play') {
-            const gamesToPlay = this.state.myGames.filter( (game) => !game.played)
-            this.setState({gamesToPlay})
-        }
-        this.setState({filter: filter})
-    }
-
     render() {
-        console.log('this.props myGamesContainer', this.props)
-        const {selectFilter} = this
-    
-        // let myFilteredGames = []
-    
-        // if (filter === 'all') {
-        //   myFilteredGames = this.state.myGames
-        // } else if (filter === 'played') {
-        //   myFilteredGames = this.state.gamesPlayed
-        // } else if (filter === 'to play') {
-        //   myFilteredGames = this.state.gamesToPlay
-        // }    
-
         return(
             <section className="ui segment myGames-container">
                 <Header text='Votre to play list' />
-                <div className="filter-container">
-                    <Button className="filter-button active" onClick={selectFilter.bind(this, 'all')} text="Tous" />
-                    <Button className="filter-button" onClick={selectFilter.bind(this, 'played')} text="Joué" />
-                    <Button className="filter-button" onClick={selectFilter.bind(this, 'to play')} text="A jouer" />
-                </div>
+                
+                <Filters {...this.props} />
 
                 <MyGamesList {...this.props} />
 
@@ -58,7 +25,7 @@ class MyGames extends Component {
 
 const mapStateToProps = (state) => state
 
-export default connect(mapStateToProps, { checkGame })(MyGames)
+export default connect(mapStateToProps, { checkGame, filterMyGames })(MyGames)
 
 
 
