824b48a5599897949e5cc95e27a6574b0c60a5c4
Adamska <adams.roy27@gmail.com>
2017-10-09 18:18:42 +0200

    refactoring the reducer

--- src/Redux/reducers/filterStatus.js
+++ src/Redux/reducers/filterStatus.js
@@ -1,14 +1,11 @@
 import { FILTER_MYGAMES } from '../actions'
 
 function filterStatus(state = 'all', action) {
-    switch(action.type) {
-      case FILTER_MYGAMES:
-        if (action.filterStatus === 'played') state = 'played'
-        else if (action.filterStatus === 'to play') state ='to play'
-        else state = 'all'
-      default:
-        return state
-    }
+  if (action.type === FILTER_MYGAMES) {
+      return action.filterStatus
+  } else {
+    return state      
+  }
 }
 
 export default filterStatus
