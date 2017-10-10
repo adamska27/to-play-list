02b06bf9597204dd139f9f394270a09e15228787
Adamska <adams.roy27@gmail.com>
2017-10-10 14:58:37 +0200

    install redux-localstorage and use it in createStore methode

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import persistState from 'redux-localstorage'
 import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
 
+const enhancer = compose(
+    applyMiddleware(thunk),
+    persistState()
+)
+
 let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
- window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
+ window.__REDUX_DEVTOOLS_EXTENSION__(), enhancer)
 
 export default store
