fcf424e1650a3cca1dab840ecb903ae2ce0fe80e
Adamska <adams.roy27@gmail.com>
2017-10-10 15:25:22 +0200

    create file .test.js and add very simple test for Form and Filters

--- src/components/Filters.test.js
+++ src/components/Filters.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Filters from './Filters'
 
-it('renders without crashing', () => {
+it('renders without crashing ', () => {
     const div = document.createElement('div')
     ReactDom.render(<Filters />, div)
 })
